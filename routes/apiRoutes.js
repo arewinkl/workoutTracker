584fa226ccd031c950b9308c5d0b8f4c4d14de00
A Joseph <alecrewinkel@gmail.com>
2020-11-17 21:18:12 -0700

    apiroutes-update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,14 +12,17 @@ module.exports = function(app) {
         });
     });
 
+    // route to update the workout entry
     app.put("/api/workouts/:id", function(req, res){
-        db.Workout.findOneAndUpdate({"_id":req.params.id}, {$push: {"exercise": req.body}})
-        .then(function(dbData){
-            res.json(dbData);
-        })
-        .catch(function(err){
-            res.json(err);
-        })
+        db.Workout.findOneAndUpdate({_id: req.params.id}, {$push: {exercises: req.body},},
+            (error, data) => {
+                if (error) {
+                    res.json(error);
+                }else {
+                    res.json(data);
+                }
+            });
+       
         
     });
 
@@ -36,11 +39,11 @@ module.exports = function(app) {
     //make route with a range
     app.get("/api/workouts/range", function(req, res){
         db.Workout.find({})
-        .then(function(req, res){
+        .then(function(dbData){
             res.json(dbData);
         })
         .catch(function(err){
             res.json(err);
         });
     });
-}
+};
